068e80dafb5660aa29fdbd4d00a6a1d379c65a24
byoso <emailvinz@gmail.com>
2023-08-28 18:04:16 +0200

    static and media files

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Branches
 - 01 : basic project
 - 02 : docker setup a Dockerfile and a docker-compose.yml
 - 03 : setup environment variables, postgres and management
+- 04 : static and mediafiles
--- _project/settings.py
+++ _project/settings.py
@@ -136,8 +136,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = ['static/', ]
+
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static', ]
+MEDIA_URL = 'media/'
+if DEBUG:
+    STATIC_ROOT = 'staticfiles/'
+    MEDIA_ROOT = 'mediafiles/'
+else:
+    STATIC_ROOT = '/vol/web/static/'
+    MEDIA_ROOT = '/vol/web/media/'
 
 
 # Default primary key field type
